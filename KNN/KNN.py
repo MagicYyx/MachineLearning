7b0721b40795152a2eb4f7ca6ba10cfe3ef9401f
MagicYyx <493055901@qq.com>
2019-06-10 14:39:10 +0800

    2019/6/10 14:39

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import numpy as np
+import pandas as pd
 
-def createDataSet():
-    group = np.array([[1, 101], [5, 89], [108, 5], [115, 8]])
-    labels = ['爱情片', '爱情片', '动作片', '动作片']
-    # labels = ['1', '2', '3', '4']
-    return group, labels
 
-def classify0(inX, dataSet, labels, k):
+def getDataSet(file_path):
+    data = pd.read_excel(file_path)
+    # 标签放在最后一列
+    x_heads = data.columns[:-1]
+    y_heads = data.columns[-1]
+    x = np.array(data[x_heads])
+    y = np.array(data[y_heads])
+    return x, y
+
+
+def classify(classify_item, k, file_path):
+    dataSet, labels = getDataSet(file_path)
     # 计算距离
-    distance = np.sum((inX - dataSet) ** 2, axis=1) ** 0.5
+    distance = np.sum((classify_item - dataSet) ** 2, axis=1) ** 0.5
     # k个最近的标签
     k_labels = [labels[index] for index in distance.argsort()[0: k]]
     # 出现次数最多的标签即为最终类别
     result = max(k_labels, key=k_labels.count)
     return result
-
-if __name__ == '__main__':
-    group, labels = createDataSet()
-    print(classify0([1,3], group, labels, 3))
