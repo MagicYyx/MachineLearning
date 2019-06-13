0f6b67588513e974cda70ca42e7fc93a63554b99
MagicYyx <493055901@qq.com>
2019-06-13 17:10:30 +0800

    new

--- DataClean.py
+++ DataClean.py
@@ -2,10 +2,27 @@
 # -*- coding:utf-8 -*-
 
 '''
-数据清洗哇哈哈哈！
+数据清洗
 '''
 
-__authore__ = 'Magic Yyx'
+__author__ = 'Magic Yyx'
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
 
 class data_clean(object):
-    pass
+    def __init__(self, path):
+        self.name = 'Magic Yyx'
+
+    # 归一化和标准化
+    def normalization(self, data, type):
+        '''
+        data按列做标准或归一，所以data要形如DataFrame或[[1,2],[3,4]]
+        :param type: "minmax" or "z-score"
+        :return:
+        '''
+        if type == 'minmax': norm = MinMaxScaler()
+        if type == 'z-score': norm = StandardScaler()
+        return  norm.fit_transform(data)
--- tt.py
+++ tt.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-from RandomForest.RandomForest import RandomForest
-from DecisionTree.DecisionTree import DecisionTree
+class commodity(object):
+    def __init__(self, id, name, price):
+        self.id = id
+        self.name = name
+        self.price = price
 
-path = r'C:\Users\Administrator\Desktop\myGit\MachineLearning\DecisionTree\Titanic.xlsx'
-x = [[3, 1, 1, 7.2]]
-dt = DecisionTree()
-rf = RandomForest()
-max_depth=[2,3,4,5,6]
-min_samples_split=[2,4,6,8]
-min_samples_leaf=[2,4,8,10,12]
-dt.fit(path, 'Classifier', max_depth, min_samples_split, min_samples_leaf)
-print(dt.predict(x))
-rf.fit(path, 'Classifier')
-print(rf.predict(x))
+    def change_price(self, new_price):
+        self.price = new_price
+
+    def print_price(self):
+        print(self.price)
