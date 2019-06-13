4a37cff57c2f9aef9317c30c3b48aa3a43f75612
MagicYyx <493055901@qq.com>
2019-06-13 17:49:56 +0800

    new

--- tt.py
+++ tt.py
@@ -1,13 +1,2 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-class commodity(object):
-    def __init__(self, id, name, price):
-        self.id = id
-        self.name = name
-        self.price = price
-
-    def change_price(self, new_price):
-        self.price = new_price
-
-    def print_price(self):
-        print(self.price)
